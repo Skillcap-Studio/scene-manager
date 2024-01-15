1ff926424d7a1a72b66945cf39f107539ad51f42
dodoveloper <davide.cor94@gmail.com>
2024-01-15 14:52:57 +0100

    feat: support custom animation scene for transition

--- addons/scene_manager/SceneManager.gd
+++ addons/scene_manager/SceneManager.gd
@@ -9,8 +9,9 @@ var is_transitioning := false
 @onready var _tree := get_tree()
 @onready var _root := _tree.get_root()
 @onready var _current_scene := _tree.current_scene
-@onready var _animation_player : AnimationPlayer = $AnimationPlayer
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 @onready var _shader_blend_rect : ColorRect = $CanvasLayer/ColorRect
+@onready var _animation_player : AnimationPlayer = $AnimationPlayer
 
 var default_options := {
 	"speed": 2,
@@ -23,6 +24,7 @@ var default_options := {
 	"skip_scene_change": false,
 	"skip_fade_out": false,
 	"skip_fade_in": false,
+	"animation_name": null,
 	"on_tree_enter": func(scene): null,
 	"on_ready": func(scene): null,
 	"on_fade_out": func(): null,
@@ -37,6 +39,7 @@ var default_options := {
 
 var singleton_entities := {}
 var _previous_scene = null
+var _user_animation_player: AnimationPlayer
 
 func _ready() -> void:
 	_set_singleton_entities()
@@ -85,6 +88,10 @@ func _get_final_options(initial_options: Dictionary) -> Dictionary:
 	for ease_key in ["ease_enter", "ease_leave"]:
 		if not ease_key in options:
 			options[ease_key] = options["ease"]
+	
+	for animation_name_key in ["animation_name_enter", "animation_name_leave"]:
+		if not animation_name_key in options:
+			options[animation_name_key] = options["animation_name"]
 
 	return options
 
@@ -137,36 +144,80 @@ func _replace_scene(path: String, options: Dictionary) -> void:
 func fade_out(setted_options: Dictionary= {}) -> void:
 	var options = _get_final_options(setted_options)
 	is_transitioning = true
-	_animation_player.speed_scale = options["speed"]
+	if not options["animation_name_enter"]:
+		_animation_player.speed_scale = options["speed"]
 
-	_shader_blend_rect.material.set_shader_parameter(
-		"dissolve_texture", options["pattern_enter"]
-	)
-	_shader_blend_rect.material.set_shader_parameter("fade", !options["pattern_enter"])
-	_shader_blend_rect.material.set_shader_parameter("fade_color", options["color"])
-	_shader_blend_rect.material.set_shader_parameter("inverted", false)
-	var animation = _animation_player.get_animation("ShaderFade")
-	animation.track_set_key_transition(0, 0, options["ease_enter"])
-	_animation_player.play("ShaderFade")
+		_shader_blend_rect.material.set_shader_parameter(
+			"dissolve_texture", options["pattern_enter"]
+		)
+		_shader_blend_rect.material.set_shader_parameter("fade", !options["pattern_enter"])
+		_shader_blend_rect.material.set_shader_parameter("fade_color", options["color"])
+		_shader_blend_rect.material.set_shader_parameter("inverted", false)
+		var animation = _animation_player.get_animation("ShaderFade")
+		animation.track_set_key_transition(0, 0, options["ease_enter"])
+		_animation_player.play("ShaderFade")
 
-	await _animation_player.animation_finished
+		await _animation_player.animation_finished
+	else:
+		assert(_user_animation_player is AnimationPlayer, "No animation player was set.")
+		_user_animation_player.speed_scale = options["speed"]
+		_user_animation_player.play(options["animation_name_enter"])
+		await _user_animation_player.animation_finished
+	
 	fade_complete.emit()
 	options["on_fade_out"].call()
 
 func fade_in(setted_options: Dictionary = {}) -> void:
 	var options = _get_final_options(setted_options)
-	_animation_player.speed_scale = options["speed"]
-	_shader_blend_rect.material.set_shader_parameter(
-		"dissolve_texture", options["pattern_leave"]
-	)
-	_shader_blend_rect.material.set_shader_parameter("fade", !options["pattern_leave"])
-	_shader_blend_rect.material.set_shader_parameter("fade_color", options["color"])
-	_shader_blend_rect.material.set_shader_parameter("inverted", options["invert_on_leave"])
-	var animation = _animation_player.get_animation("ShaderFade")
-	animation.track_set_key_transition(0, 0, options["ease_leave"])
-	_animation_player.play_backwards("ShaderFade")
+	if not options["animation_name_leave"]:
+		if options["animation_name_enter"]:
+			_user_animation_player.play("RESET")
+		
+		_animation_player.speed_scale = options["speed"]
+		_shader_blend_rect.material.set_shader_parameter(
+			"dissolve_texture", options["pattern_leave"]
+		)
+		_shader_blend_rect.material.set_shader_parameter("fade", !options["pattern_leave"])
+		_shader_blend_rect.material.set_shader_parameter("fade_color", options["color"])
+		_shader_blend_rect.material.set_shader_parameter("inverted", options["invert_on_leave"])
+		var animation = _animation_player.get_animation("ShaderFade")
+		animation.track_set_key_transition(0, 0, options["ease_leave"])
+		_animation_player.play_backwards("ShaderFade")
 
-	await _animation_player.animation_finished
+		await _animation_player.animation_finished
+	else:
+		assert(_user_animation_player is AnimationPlayer, "No animation player was set.")
+		_user_animation_player.speed_scale = options["speed"]
+		_user_animation_player.play_backwards(options["animation_name_leave"])
+		await _user_animation_player.animation_finished
+	
 	is_transitioning = false
 	transition_finished.emit()
 	options["on_fade_in"].call()
+
+
+func set_animation_player(animation_player) -> void:
+	assert(
+		animation_player is String or animation_player is PackedScene,
+		"set_animation_player() must receive a string (path to AnimationPlayer.tscn) or a PackedScene"
+	)
+	var loaded_animation_player = _load_resource(animation_player).instantiate()
+	assert(
+		loaded_animation_player is AnimationPlayer,
+		(
+			"The scene loaded from set_animation_player() (%s) must receive an AnimationPlayer"
+			% _user_animation_player
+		)
+	)
+	
+	if _user_animation_player is AnimationPlayer:
+		_user_animation_player.queue_free()
+	_user_animation_player = loaded_animation_player
+	canvas_layer.add_child(_user_animation_player)
+	_user_animation_player.play("RESET")
+
+
+func _load_resource(resource) -> Resource:
+	if resource is PackedScene:
+		return resource
+	return ResourceLoader.load(resource)
